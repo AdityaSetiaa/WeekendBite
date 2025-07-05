d24a9b99f3ef5fad367156955c11888095c411e5
Adetia <adityaxsetia@gmail.com>
2025-07-06 01:16:53 +0530

    modified

--- src/Components/DisplayFoods.tsx
+++ src/Components/DisplayFoods.tsx
@@ -78,7 +78,7 @@ function DisplayFoods() {
     <div className="w-auto h-auto bg-gray-100 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-5  items-center justify-center py-10">
     
       {
-         filteredFoods.map((food: any) =>(
+         (tag.length === 0 || tag[0] === "") ? filteredFoods.map((food: any) =>(
             <div key={food.id} className='flex flex-col items-center justify-center w-auto h-full bg-gray-100 transition-transform duration-300 ease-in-out cursor-pointer '>
                 <div className='flex flex-col items-center justify-center w-full mt-4 h-full hover:text-red-400 duration-200'>
                     <img src={food.image} alt={food.name} className="w-68 h-68 object-cover rounded-md " />
@@ -92,8 +92,22 @@ function DisplayFoods() {
                 </div>
                 
             </div>
+        )) : foods.foods
+          .map((food: any) =>(
+            <div key={food.id} className='flex flex-col items-center justify-center w-auto h-full bg-gray-100 transition-transform duration-300 ease-in-out cursor-pointer '>
+                <div className='flex flex-col items-center justify-center w-full mt-4 h-full hover:text-red-400 duration-200'>
+                    <img src={food.image} alt={food.name} className="w-68 h-68 object-cover rounded-md " />
+                    <h1 className=' text-lg mt-2 text-center '>{food.name}</h1>
+                    <span>{food.Credits.map((credit: any, index: number) => (
+                        <span key={index}>
+                            <a href={credit.link} className='hover:text-red-400'>by {credit.name}</a>
+                            {index < food.Credits.length - 1 && ', '}
+                        </span>
+                    ))}</span>
+                </div>
+            </div>
         ))
-      }
+      } 
     </div>
   </>
   )
