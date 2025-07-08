6b0f4b53dc61d1d302ef52f6d91a402d8c47c5df
Adetia <adityaxsetia@gmail.com>
2025-07-08 09:37:17 +0530

    header update

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -7,7 +7,7 @@ function Header() {
       <div className='w-full h-27 border-2 border-gray-300 bg-white shadow-md flex items-center justify-between px-4'>
         <img src={week} alt="Recefied Logo" className='h-32 flex flex-col justify-center items-center' />
 
-        <div className='mr-4'>
+        <div className='mr-4 hover:text-red-400 cursor-pointer'>
           About us
         </div>
         
